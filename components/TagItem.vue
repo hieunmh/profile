ca5bf0ac7b26d8448edeba2176f4ca2aa1082a6a
hieuunm <hieunm.pt@gmail.com>
2023-08-28 14:38:52 +0700

    change tech tag

--- components/TagItem.vue
+++ components/TagItem.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="flex items-center gap-5 cursor-pointer w-full h-full py-5">
-        <div class="w-full relative">
+    <div class="flex items-center gap-4 cursor-pointer w-full h-full py-5">
+        <div class="w-full relative pb-5">
             <div>
                 <h4 class="text-xl font-bold text-center">
-                    <Icon :name="el.icon" size="70" color="" class="dark:fill-white mr-5 ml-1 p-3" />
+                    <Icon :name="el.icon" size="70" color="" class="dark:fill-white mr-5 ml-1 p-3 w-full" />
                     <span class="">{{ el.name }}</span>
                 </h4>
             </div>
 
-            <div class="absolute top-0 rounded-lg hover:opacity-80 bg-gray-300 w-full h-full z-10 opacity-0 text-gray-950">
+            <div class="absolute top-0 rounded-lg hover:opacity-80 sm:w-[70%] sm:ml-[15%] w-full ml-0
+                bg-gray-300 h-full z-10 opacity-0 text-gray-950"
+            >
                 <div class="flex justify-center items-center h-full font-bold text-gray-950 text-3xl">
                     {{ el.percent }}%
                 </div>
